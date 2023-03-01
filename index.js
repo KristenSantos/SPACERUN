4546c478586d5e1049b04e28b594e5652c4fb7d7
KristenSantos <Kristensxntos@gmail.com>
2023-03-01 14:36:53 -0500

    created game canvas

--- index.js
+++ index.js
@@ -43,3 +43,30 @@ function gamePaused() {
 function mainMenu() {
     window.location.reload();
 }
+//////////
+//LOGIC//
+////////
+
+let gamePiece;
+function startGame() {
+    gameArea.start(); 
+    gamePiece = new gameBall();
+}
+///////////////////
+//GAME AREA LOAD//
+/////////////////
+
+let gameArea = {
+    canvas : document.createElement('canvas'),
+    start : function() {
+        this.canvas.width = screenWidth;
+        this.canvas.height = window.innerHeight;
+        this.canvas.style.backgroundColor = 'none';
+        this.context = this.canvas.getContext('2d');
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        requestAnimationFrame(updateGameArea);
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+};
