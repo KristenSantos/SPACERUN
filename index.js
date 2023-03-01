ccc25e34b103935a6fe7066cd88d48ada40e0852
JackieGL <jlopez@aoiths.org>
2023-03-01 19:29:58 +0000

    .

--- index.js
+++ index.js
@@ -1,125 +1,18 @@
-// ///////////////////
-// ///Ball Control///
-// /////////////////
+let homePage = document.getElementById('main');
+let homeBut = document.getElementById('home');
+let pauseBut = document.getElementById('pause');
+let pauseMain = document.getElementById('pause-main');
+let pausePage = document.getElementById('game-paused');
+let gameTap = document.getElementById('balltap');
+let gameOver = document.getElementById('gameover');
+let overMessage = document.getElementById('game-over');
+let showHighScore = document.querySelector('#high-score div');
+let screenWidth = window.innerWidth > 600 ? 540 : window.innerWidth;
+let screenWidthStyle = 'width:' + String(screenWidth) + 'px';
 
-const ball = document.querySelector('#ball');
-const ballSection = document.querySelector('#ballSection');
-const body = document.querySelector('body')
-
-// Set the initial position and velocity of the ball
-let ballPosition = 0;
-let ballVelocity = 0;
-
-// Define a function to update the position of the ball
-function updateBallPosition() {
-  ball.style.bottom = ballPosition + 'px';
-}
-
-// Add an event listener for the space bar keydown event
-document.addEventListener('keydown', function(event) {
-  if (event.code === 'Space') {
-    ballVelocity = 5;
-  }
-});
-
-// Add an event listener for the space bar keyup event
-document.addEventListener('keyup', function(event) {
-  if (event.code === 'Space') {
-    ballVelocity = -2;
-  }
-});
-
-// Define a function to update the position of the ball based on its velocity
-function updateBallVelocity() {
-  ballPosition += ballVelocity;
-  if (ballPosition < 0) {
-    ballPosition = 0;
-    ballVelocity = 0;
-  }
-  updateBallPosition();
-
-  // Check if the ball has reached the top of the ball section and move the section up if it has
-  if (ballPosition > (ballSection.offsetHeight - ball.offsetHeight)) {
-    ballSection.style.top = -(ballPosition - (ballSection.offsetHeight - ball.offsetHeight)) + 'px';
-  }
-}
- 
-// // Set an interval to update the position of the ball every 20 milliseconds
-setInterval(updateBallVelocity, 20);
-
-///////////////////
-//Obstcle Control/
-/////////////////
-let rectangles = []
-let isGameOver = true
-
-class Rectangle {
-    constructor(newRectangleBottom){
-      this.bottom = newRectangleBottom
-      this.left = Math.random() * 1430
-      this.visual = document.createElement('div')
-      this.direction = 'left'
-      this.speed = 5 // adjust speed as needed
-  
-      // rest of the constructor code
-      const visual = this.visual
-      visual.classList.add('rectangleObst')
-      visual.style.left = this.left + 'px'
-      visual.style.bottom = this.bottom + 'px';
-      grid.appendChild(visual)
-    }
-  }
-  
-  
-  function createRectangleObst() {
-    for(let i = 0; i < RectangleObstCount; i++){
-      let RectangleObstGap = 600 / RectangleObstCount
-      let newRectangleBottom = 200 + i * RectangleObstGap;
-      let newRectangleObs = new Rectangle(newRectangleBottom)
-      rectangles.push(newRectangleObs)
-      console.log(rectangles)
-  
-    }
-  }
-
-  function moveRectangleObst() {
-    if(rocketBottomSpace > 200){
-      rectangles.forEach(rectangle => {
-        if (rectangle.direction === 'left') {
-          rectangle.left -= rectangle.speed
-        } else {
-          rectangle.left += rectangle.speed
-        }
-        let visual = rectangle.visual
-        visual.style.left = rectangle.left + 'px'
-  
-        // change direction if rectangle hits left or right edge of grid
-        if (rectangle.left <= 0) {
-          rectangle.direction = 'right'
-        } else if (rectangle.left >= (grid.offsetWidth - visual.offsetWidth)) {
-          rectangle.direction = 'left'
-        }
-  
-        // rest of the function code
-        if(rectangle.bottom < 10){
-          let firstRectangle = rectangles[0].visual
-          firstRectangle.classList.remove('retangle')
-          rectangles.shift()
-          let newRectangleObs = new Rectangle(600)
-          rectangles.push(newRectangleObs)
-        }
-  
-      })
-    }
-  }
-
-  function startGame() {
-    if(!isGameOver){
-      createRectangleObst()
-      createRocket()
-      setInterval(moveRectangleObst, .1)
-      // fly()
-    }
-   
-  }
-  startGame()
+pauseMain.style.cssText = screenWidthStyle;
+homePage.style.cssText = screenWidthStyle;
+pauseBut.style.cssText = screenWidthStyle;
+homeBut.style.cssText = screenWidthStyle;
+pausePage.style.cssText  = screenWidthStyle;
+overMessage.style.cssText = screenWidthStyle;
